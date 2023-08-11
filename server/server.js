102647f84192b3b822248f3a55d9b8081727ede3
Jamil Barrett <jamilbarrett@jamils-imac.myfiosgateway.com>
2023-08-11 14:43:01 -0400

    basic server created

--- server/server.js
+++ server/server.js
@@ -1,3 +1,21 @@
+require('dotenv').config()
 const express = require('express')
+const cookieParser = require('cookie-parser')
+
 
 const app = express()
+const db = require('./db/connection')
+const PORT = process.env.PORT || 3333
+
+// import routes
+const api_routes = require('./routes/api_routes')
+
+// middleware
+app.use(express.json())
+app.use(cookieParser())
+
+// ensure the db is open/start the server
+db.once('open', () => {
+    // start express
+    app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+  });
