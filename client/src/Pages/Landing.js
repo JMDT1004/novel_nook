96e68149d5fcad35672171c2f5ebc291a3793b60
Jamil Barrett <jamilbarrett@jamils-imac.myfiosgateway.com>
2023-08-12 17:51:46 -0400

    search user/favorites

--- client/src/Pages/Landing.js
+++ client/src/Pages/Landing.js
@@ -1,26 +1,44 @@
 import { useEffect, useState } from 'react';
 
 function UserList() {
-    const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState([])
+    const [selectedUser, setSelectedUser] = usestate('')
+
 
     useEffect(() => {
         fetch('/api/users')
             .then(response => response.json())
             .then(data => setUsers(data))
-            .catch(error => console.error(error));
-    }, []);
+            .catch(error => console.error(error))
+    }, [])
+
+    const handleUserClick = user => {
+        setSelectedUser(user)
+    }
 
     return (
         <>
             <h2>User List</h2>
             <ul>
                 {users.map(user => (
-                    <li key={user.id}>
+                    <li key={user.id} onClick={() => handleUserClick(user)}>
                         <h3>{user.username}</h3>
                         <p>{user.email}</p>
                     </li>
                 ))}
             </ul>
+
+            {selectedUser && (
+                <div>
+                    <h2>{selectedUser.username}'s Favorite Books</h2>
+                    <ul>
+                        {selectedUser.favoriteBooks.map((book,index => (
+                            <li key={index}>{book}</li>
+                        )))}
+                    </ul>
+            
+                </div>
+            )}
         </>
     );
 }
