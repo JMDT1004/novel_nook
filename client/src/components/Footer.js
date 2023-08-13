7b5688972022c67d3da731271364bb7ea790be0e
JT <raikiri24@gmail.com>
2023-08-13 11:47:05 -0400

    updated footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,8 +1,37 @@
+import React from 'react';
 
-function Footer () {
+function Footer() {
     return (
-        <>
-        </>
+        <footer className="footer">
+            <div>
+                <a
+                    href="https://github.com/"
+                    target="_blank"
+                    rel="noopener noreferrer">
+
+                </a>
+                <a
+                    href="https://github.com/"
+                    target="_blank"
+                    rel="noopener noreferrer">
+
+                </a>
+                <a
+                    href="https://github.com/"
+                    target="_blank"
+                    rel="noopener noreferrer">
+
+                </a>
+                <a
+                    href="https://github.com/"
+                    target="_blank"
+                    rel="noopener noreferrer">
+
+                </a>
+            </div>
+            <p>&copy; {new Date().getFullYear()}. All rights reserved.</p>
+        </footer>
     )
 }
-export default Footer
+
+export default Footer;
