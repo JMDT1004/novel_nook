b0a55d2c432c52f2a1876f60a0f7a5591990ce54
Jeremy Boyer <jeremytboyer@gmail.com>
2023-08-15 10:43:12 -0400

    commit before pull

--- client/src/Pages/Book.js
+++ client/src/Pages/Book.js
@@ -14,14 +14,13 @@ function Book(props) {
       });
   }, []);
 
-  async function addToFavorites(){
-    await axios.post('/api/favorites', {
-        bookId: data.book.id,
-        image: data.book.volumeInfo.imageLinks.thumbnail,
-        title: data.book.volumeInfo.title,
-
+  async function addToFavorites() {
+    await axios.post("/api/favorites", {
+      bookId: data.book.id,
+      image: data.book.volumeInfo.imageLinks.thumbnail,
+      title: data.book.volumeInfo.title,
     });
-    navigate('/dashboard');
+    navigate("/dashboard");
   }
 
   return (
@@ -30,7 +29,7 @@ function Book(props) {
       <h1>{data.book.volumeInfo?.title}</h1>
       <h2>{data.book.volumeInfo?.authors}</h2>
       <p>{data.book.volumeInfo?.description}</p>
-      <a href="{data.book.volumeInfo?.infoLink}">More Info</a>
+      <a href={data.book.volumeInfo?.infoLink}>More Info</a>
       {props.state.user && (
         <button onClick={addToFavorites}>Add to Favorites</button>
       )}
