0cd65c94c93310a4745ade80a8ade43c4fe2eb3e
Jonathan Goncalves <jonjigoncalves@gmail.com>
2023-08-15 13:09:31 -0400

    almost done with landing

--- client/src/components/Bookcard.js
+++ client/src/components/Bookcard.js
@@ -4,24 +4,31 @@ import { NavLink } from 'react-router-dom'
 export function BookCard({ books }) {
   return (
     <>
-      <div className="flex flex-wrap">
-        {books.map((book) => (
-          <div key={book.id}>
-            <NavLink key={book.id} to={`/book/${book.id}`}>
-              <img
-                src={book.volumeInfo.imageLinks?.thumbnail}
-                alt={`${book.volumeInfo.title} cover`}
-              />
-            </NavLink>
+   <div className="grid grid-cols-5 gap-4">
+  {books.map((book) => (
+    <div key={book.id} className="p-2 border rounded-lg">
+      <NavLink key={book.id} to={`/book/${book.id}`} className="block mb-2">
+        <img
+          src={book.volumeInfo.imageLinks?.thumbnail}
+          alt={`${book.volumeInfo.title} cover`}
+          className="w-full h-full object-cover"
+        />
+      </NavLink>
 
-            <h3>{book.volumeInfo.title}</h3>
-            <p>Author(s): {book.volumeInfo.authors?.join(', ') || 'Unknown author'}</p>
+      <h3 className="text-sm font-semibold mb-1">{book.volumeInfo.title}</h3>
+      <p className="text-xs text-gray-600">
+        Author(s): {book.volumeInfo.authors?.join(', ') || 'Unknown author'}
+      </p>
 
-            {/* <p>Description: {book.volumeInfo.description || 'No description available'}</p> */}
+      {/* Uncomment this section if you want to show book descriptions */}
+      {/* <p className="text-xs mt-1">
+        {book.volumeInfo.description || 'No description available'}
+      </p> */}
+    </div>
+  ))}
+</div>
 
-          </div>
-        ))}
-      </div>
+
     </>
   );
 }
