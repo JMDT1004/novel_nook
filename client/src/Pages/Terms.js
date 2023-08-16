824baa4f3b15a824c7ac72558aaf68e833670cab
Jonathan Goncalves <jonjigoncalves@gmail.com>
2023-08-16 16:26:21 -0400

    added terms and 404

---
+++ client/src/Pages/Terms.js
@@ -0,0 +1,105 @@
+
+
+function Terms() {
+    return (
+
+        <div className="container my-24 mx-auto md:px-6 ">
+
+            <section className="mb-32 text-center">
+                <div className="flex justify-center">
+                    <div className="max-w-[800px]">
+                        <h2 className="mb-12 text-5xl font-bold tracking-tight text-black md:text-6xl xl:text-7xl">
+                            Terms <br />
+                            <span className="text-primary dark:text-primary-400">And Conditions</span>
+                        </h2>
+
+                        <div>
+                            <h3 className="text-primary text-xl dark:text-primary-400">1. Acceptance of Terms
+                            </h3>
+                            <p className="text-lg text-black pb-5">
+                                Welcome to NovelNook. By accessing and using the Website, you agree to comply with and be bound by the following terms and conditions ("Terms"). If you do not agree to these Terms, please refrain from using the Website.
+                            </p>
+                        </div>
+
+
+                        <div>
+                            <h3 className="text-primary text-xl dark:text-primary-400">2. Intellectual Property
+                            </h3>
+                            <p className="text-lg text-black pb-5">
+                                All content, including but not limited to text, images, graphics, logos, and trademarks, displayed on NovelNook is the property of NovelNook or its licensors and is protected by applicable intellectual property laws. You are granted limited, non-exclusive, and revocable permission to access and use the content for personal, non-commercial purposes.
+                            </p>
+                        </div>
+
+                        <div>
+                            <h3 className="text-primary text-xl dark:text-primary-400">3. User Accounts
+                            </h3>
+                            <p className="text-lg text-black pb-5">
+                                To access certain features of NovelNook, you may need to create a user account. You are responsible for maintaining the confidentiality of your account information, including your password. You agree to promptly update any information provided to NovelNook to ensure accuracy.
+                                </p>   
+                        </div>
+
+                        <div>
+                            <h3 className="text-primary text-xl dark:text-primary-400">4. Privacy
+                            </h3>
+                            <p className="text-lg text-black pb-5">
+                                Your use of NovelNook is also governed by our Privacy Policy, which outlines how we collect, use, and disclose your personal information. By using NovelNook, you consent to the practices described in the Privacy Policy.
+                            </p>
+                        </div>
+
+                        <div>
+                            <h3 className="text-primary text-xl dark:text-primary-400">5. Content Submission
+                            </h3>
+                            <p className="text-lg text-black pb-5">
+                                If you choose to submit content to NovelNook, such as reviews, comments, or user-generated content, you grant NovelNook a non-exclusive, royalty-free, perpetual, irrevocable, and fully sublicensable right to use, reproduce, modify, adapt, publish, translate, create derivative works from, distribute, and display such content worldwide.
+                            </p>
+                        </div>
+
+                        <div>
+                            <h3 className="text-primary text-xl dark:text-primary-400">6. Orders and Purchases
+                            </h3>
+                            <p className="text-lg text-black pb-5">
+                                All purchases made through NovelNook are subject to our Return and Refund Policy. By placing an order, you agree to the terms and conditions outlined in these documents.
+                            </p>
+                        </div>
+
+                        <div>
+                            <h3 className="text-primary text-xl dark:text-primary-400">7. Disclaimers
+                            </h3>
+                            <p className="text-lg text-black pb-5">
+                                The content on NovelNook is provided for informational purposes only. NovelNook makes no representations or warranties about the accuracy, completeness, or reliability of any information on the Website. Your use of NovelNook is at your own risk.
+                            </p>
+                        </div>
+
+                        <div>
+                            <h3 className="text-primary text-xl dark:text-primary-400">8. Limitation of Liability
+                            </h3>
+                            <p className="text-lg text-black pb-5">
+                                In no event shall NovelNook be liable for any direct, indirect, incidental, special, consequential, or punitive damages arising out of or in connection with your use of the Website.
+                            </p>
+                        </div>
+
+                        <div>
+                            <h3 className="text-primary text-xl dark:text-primary-400">9. Indemnification
+                            </h3>
+                            <p className="text-lg text-black pb-5">
+                                You agree to indemnify and hold NovelNook harmless from any claims, losses, liabilities, expenses, or damages, including attorney's fees, arising out of or in connection with your use of the Website or violation of these Terms.
+                            </p>
+                        </div>
+
+                        <div>
+                            <h3 className="text-primary text-xl dark:text-primary-400">10. Governing Law
+                            </h3>
+                            <p className="text-lg text-black pb-5">
+                                These Terms are governed by and construed in accordance with the laws of New Jersey. Any disputes arising from or related to these Terms shall be subject to the exclusive jurisdiction of the courts in  New Jersey.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </div>
+
+    );
+}
+
+export default Terms;
