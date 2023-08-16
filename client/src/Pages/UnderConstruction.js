01b1a77ffd849dfa4cb1af3ef4883db1f10ad2b9
Jonathan Goncalves <jonjigoncalves@gmail.com>
2023-08-16 16:35:59 -0400

    added under maintenance site

--- client/src/Pages/UnderConstruction.js
+++ client/src/Pages/UnderConstruction.js
@@ -1,10 +1,13 @@
 function UnderConstruction() {
     return (
-        <div className="flex justify-center items-center flex-col">
-            <h1 className="mt-6 text-3xl font-bold text-center mb-6">Thank you for visiting our app</h1>
-            <p className="text-center">This page is currently under construction</p>
-            <img src={"../assets/cone.png"} alt="cone" />
+        <div className="flex  justify-center py-20">
+        <div className="flex flex-col items-center justify-center max-w-2xl">
+          <img src="https://cdn.pixabay.com/photo/2017/06/20/08/12/maintenance-2422173__340.png"
+            alt="tailwindcss maintenance"/>
+          <h1 className="mb-3 text-5xl font-bold text-center text-purple-600">Under Maintenance</h1>
+          <p className="text-center text-4xl text-gray-600">The Page you are looking for is currently under maintenance.</p>
         </div>
+      </div>
     );
 }
 
